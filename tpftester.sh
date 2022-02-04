3a83be28a25474359ab9c7d5e33cbd7cf7e106c5
Theo HERBRETEAU <therbret@gmx.comwq>
2022-02-04 18:27:10 +0100

    wip new subject

--- tpftester.sh
+++ tpftester.sh
@@ -2,16 +2,16 @@
 
 case $1 in
  -l)
-  gcc libftprintf.a therbret_main_test_printf.c -D L -D PRINTF=ft_printf && ./a.out > ft_output.txt
-  gcc libftprintf.a therbret_main_test_printf.c -D L && ./a.out > stdio_output.txt
+  gcc therbret_main_test_printf.c -D L -L. -lftprintf -D PRINTF=ft_printf -o ft.out && ./ft.out > ft_output.txt
+  gcc therbret_main_test_printf.c -D L -o stdio.out && ./stdio.out > stdio_output.txt
   ;;
  -f)
-  gcc -g -fsanitize=address libftprintf.a therbret_main_test_printf.c -D PRINTF=ft_printf && ./a.out > ft_output.txt
-  gcc -g -fsanitize=address libftprintf.a therbret_main_test_printf.c && ./a.out > stdio_output.txt
+  gcc -g -fsanitize=address therbret_main_test_printf.c -L. -lftprintf -D PRINTF=ft_printf -o ft.out && ./ft.out > ft_output.txt
+  gcc -g -fsanitize=address therbret_main_test_printf.c -o stdio.out  && ./stdio.out > stdio_output.txt
   ;;
  *)
-  gcc libftprintf.a therbret_main_test_printf.c -D PRINTF=ft_printf && ./a.out > ft_output.txt
-  gcc libftprintf.a therbret_main_test_printf.c && ./a.out > stdio_output.txt
+  gcc therbret_main_test_printf.c -L. -lftprintf -D PRINTF=ft_printf -o ft.out && ./ft.out > ft_output.txt
+  gcc therbret_main_test_printf.c -o stdio.out && ./stdio.out > stdio_output.txt
   ;;
 esac
 diff -a stdio_output.txt ft_output.txt > diff.txt
